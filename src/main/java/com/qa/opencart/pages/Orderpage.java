7258489e492f1803a862de18856e065b6b12d774
madhavi2023 <madhu8004@gmail.com>
2023-03-13 04:17:49 -0400

    orederpage

---
+++ src/main/java/com/qa/opencart/pages/Orderpage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Orderpage {
+	By order = By.id("order");
+	public void getOrder(){
+		System.out.println("order page");
+	}
+	
+
+}
